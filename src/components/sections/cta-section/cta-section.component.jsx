967940d5648c79234a07110d6db21a462cf52dc1
remco-jonk <remcojonk@hotmail.com>
2024-01-17 09:04:43 +0100

    responsive cta section

--- src/components/sections/cta-section/cta-section.component.jsx
+++ src/components/sections/cta-section/cta-section.component.jsx
@@ -4,7 +4,7 @@ import Button from '../../button/button.component'
 const CtaSection = () => {
   return (
     <section className="cta-container">
-      <h2>Benieuwd wat wij voor u kunnen betekenen?</h2>
+      <h2 className='cta-title'>Benieuwd wat wij voor u kunnen betekenen?</h2>
       <p className='cta-paragraph'>Neem dan vrijblijvend contact met ons op</p>
       <Button>Contact</Button>
     </section>
