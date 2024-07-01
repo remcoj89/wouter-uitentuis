c312280daf901032c2d36087f4cbe35909ae673f
remco jonk <remcojonk@hotmail.com>
2024-07-01 17:03:04 +0200

    created footer

--- ui/sections/footer-section/footer-section.component.jsx
+++ ui/sections/footer-section/footer-section.component.jsx
@@ -4,6 +4,7 @@ import Styles from './footer-section.module.css';
 
 // Hooks
 import Link from 'next/link';
+import { PhoneIcon, EmailIcon, LocationIcon } from '@/assets/icons';
 
 // Components
 
@@ -15,9 +16,16 @@ return (
   <section id="section-footer">
     <footer className={`wrapper ${Styles.footerWrapper}`}>
       <div className={Styles.footerContactWrapper}>
-      <Link href="mailto: info@wouteruitentuis.nl" target='_blank'> <IconComponent  info@wouteruitentuis.nl</Link>
-      <Link href="https://maps.app.goo.gl/fwxp3YxCLdR9xtrL6" target='_blank' >Westeinde 29, 1636VB, Schermerhorn, KVK: 12345678</Link>
-      <Link href="tel: 0630272667" target='_blank'> 06-30272667</Link>
+      <Link href="mailto: info@wouteruitentuis.nl" target='_blank'>
+        <IconComponent iconStyle='greenIcon'><EmailIcon color="#ffff"/> </IconComponent> info@wouteruitentuis.nl
+       </Link>
+      <Link href="https://maps.app.goo.gl/fwxp3YxCLdR9xtrL6" target='_blank' >
+        <IconComponent iconStyle='greenIcon' ><LocationIcon color="#ffff"/></IconComponent>
+        Westeinde 29, 1636VB, Schermerhorn, KVK: 12345678
+        </Link>
+      <Link href="tel: 0630272667" target='_blank'>
+        <IconComponent iconStyle='greenIcon'><PhoneIcon color="#ffff"/> </IconComponent>06-30272667
+       </Link>
       </div>
       <p> &copy;All Copyrights Reseverd {currentYear} </p>
     </footer>
