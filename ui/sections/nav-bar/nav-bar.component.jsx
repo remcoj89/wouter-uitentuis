4a33b485be32be30a3dd5070e6df8b208e295695
remco jonk <remcojonk@hotmail.com>
2024-10-16 10:56:50 +0200

    added function to close mobile nav when nav link is clicked

--- ui/sections/nav-bar/nav-bar.component.jsx
+++ ui/sections/nav-bar/nav-bar.component.jsx
@@ -5,28 +5,53 @@ import Styles from './nav-bar.module.css';
 // Hooks
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import {useState} from 'react'
 
 // Icons
 import { PhoneIcon, EmailIcon, CloseIcon, BurgerIcon } from '@/assets/icons';
 
 const NavBar = () => {
+   const [cancelButtonChecked, setCancelButtonChecked] = useState(false);
   const pathname = usePathname();
+
+  function handleNavLinkClick() {
+    setCancelButtonChecked(true)
+  }
 
   return (
     <header className={Styles.header}>
       <nav className={Styles.nav}>
         <div className={Styles.navWrapper}>
           <Link  className={Styles.logo} href="/">W. Uitentuis</ Link>
-          <input type="radio" name="slide" className={Styles.navButton} id={Styles.cancelButton} />
+          <input type="radio" name="slide" className={Styles.navButton} id={Styles.cancelButton} checked={cancelButtonChecked}/>
           <input type="radio" name="slide" className={Styles.navButton} id={Styles.menuButton}  />
 
           <ul className={Styles.navLinks}>
             <label className={`${Styles.navButton} ${Styles.cancelButton}`} htmlFor={Styles.cancelButton}>
               <CloseIcon color="#ffff" />
             </label>
-            <Link href='/' className={`${Styles.navLink} ${pathname === '/' ? `${Styles.active}` : ""}`} >Field management</Link>
-            <Link href='/hovenier' className={`${Styles.navLink} ${pathname === '/hovenier' ? `${Styles.active}` : ""}`}>Hovenier</Link>
-            <Link href='/groen-onderhoud' className={`${Styles.navLink} ${pathname === '/groen-onderhoud' ? `${Styles.active}` : ""}`}>Groen Onderhoud</Link>
+
+            <Link
+              href='/'
+              className={`${Styles.navLink} ${pathname === '/' ? `${Styles.active}` : ""}`}
+              onClick={handleNavLinkClick} >
+                Field management
+            </Link>
+
+            <Link
+              href='/hovenier'
+              className={`${Styles.navLink} ${pathname === '/hovenier' ? `${Styles.active}` : ""}`}
+              onClick={handleNavLinkClick}>
+                Hovenier
+            </Link>
+
+            <Link
+              href='/groen-onderhoud'
+              className={`${Styles.navLink} ${pathname === '/groen-onderhoud' ? `${Styles.active}` : ""}`}
+              onClick={handleNavLinkClick}>
+                Groen Onderhoud
+            </Link>
+
             <div className={Styles.contactWrapper}>
               <Link href="mailto: info@wouteruitentuis.nl" id={Styles.emailLink} className={Styles.contactWrapperIcon}><EmailIcon /> info@wouteruitentuis.nl</Link>
               <Link href="tel: 0630272667" id={Styles.phoneLink} className={Styles.contactWrapperIcon}><PhoneIcon /> 06-30272667</Link>
